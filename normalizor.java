39037a01a2d097c71cb231681c581fe98bae2b52
SliferTheSkyDragon <kareem.m.mohsen@gmail.com>
2017-03-25 12:01:42 +0200

    norm

--- normalizor.java
+++ normalizor.java
@@ -9,7 +9,7 @@ import java.util.List;
  */
 public class normalizor {
 
-    public List<String> run(StringBuilder sb) {
+    public static List<String> run(StringBuilder sb) {
         List<String> normal = new ArrayList<>();
         StringBuilder acc = new StringBuilder();
         for (int i = 0; i < sb.length(); i++) {
